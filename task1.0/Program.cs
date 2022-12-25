976a65e35b71459f937bdf27697d32aef34d4d6d
sofia <sonrud@mail.ru>
2022-12-25 18:12:48 +0300

    add task 34

--- task1.0/Program.cs
+++ task1.0/Program.cs
@@ -1,8 +1,9 @@
 ﻿void InputArray (int [] array)
 {
      for (int i=0; i<array.Length;i++)
-    array[i]= new Random().Next(100,150);
-}
+    array[i]= new Random().Next(100,1000);
+     Console.WriteLine($"[{string.Join(",",array)}]") ;
+     }
 
 int ReleaseArray (int [] array)
 {
@@ -14,7 +15,9 @@ int ReleaseArray (int [] array)
 }
 
 Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
 int n= Convert.ToInt32(Console.ReadLine());
 int [] array = new int [n];
 InputArray(array);
-Console.WriteLine(ReleaseArray(array));
+Console.WriteLine("количество четных элементов массива: ");
+Console.Write(ReleaseArray(array));
