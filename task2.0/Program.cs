5826cff9d7854143b70ea69d750530cbc8922cfa
sofia <sonrud@mail.ru>
2022-12-25 18:14:13 +0300

    add task 36

---
+++ task2.0/Program.cs
@@ -0,0 +1,22 @@
+﻿void InputArray (int [] array)
+{
+     for (int i=0; i<array.Length;i++)
+    array[i]= new Random().Next(-5,5);
+    Console.WriteLine($"[{string.Join(",",array)}]") ;
+}
+
+void ReleaseArray (int [] array)
+{
+    int sum = 0;
+    for (int i=1; i<array.Length;i+=2)
+    { sum=sum + array[i];} 
+    Console.WriteLine($"Сумма нечетных элементов: {sum}") ;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n= Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+InputArray(array);
+ReleaseArray(array);
+
